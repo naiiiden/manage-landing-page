bdfb2f00786d3143ab8d7f3be8cb33875088e5dd
naiden <na4ko359@gmail.com>
2022-01-20 21:29:06 +0200

    incomplete push,asking for help

---
+++ script/script.js
@@ -0,0 +1,9 @@
+document.getElementById("hamburger").addEventListener("click", () => {
+    document.getElementById("nav-ul").classList.toggle("show");
+
+    if (document.getElementById("menu-icon").getAttribute("src") === "images/icon-hamburger.svg") {
+        document.getElementById("menu-icon").setAttribute("src", "images/icon-close.svg");
+    } else {
+        document.getElementById("menu-icon").setAttribute("src", "images/icon-hamburger.svg");
+    }
+});
