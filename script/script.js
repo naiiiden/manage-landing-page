36d396950ffcf277c603f62787f4ec51a5be35fa
naiden <na4ko359@gmail.com>
2022-01-21 08:20:54 +0200

    fixed mobile navigation gap issue

--- script/script.js
+++ script/script.js
@@ -5,5 +5,5 @@ document.getElementById("hamburger").addEventListener("click", () => {
         document.getElementById("menu-icon").setAttribute("src", "images/icon-close.svg");
     } else {
         document.getElementById("menu-icon").setAttribute("src", "images/icon-hamburger.svg");
-    }
+    };
 });
