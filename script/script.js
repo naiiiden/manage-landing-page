6e9bb792968c2992aa57c741e3ea5b45a85048a7
naiden <na4ko359@gmail.com>
2022-01-21 17:21:42 +0200

    slider section completed & responsive

--- script/script.js
+++ script/script.js
@@ -7,3 +7,33 @@ document.getElementById("hamburger").addEventListener("click", () => {
         document.getElementById("menu-icon").setAttribute("src", "images/icon-hamburger.svg");
     };
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "flex";
+    dots[slideIndex-1].className += " active";
+}
